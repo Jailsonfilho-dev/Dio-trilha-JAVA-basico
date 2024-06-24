1ad742fe7367a6eb7c9a911e7a6948dcbcd2cb0c
jailsonfilho-dev <jailsonseverinofilho.@hotmail.com>
2024-06-24 20:52:56 -0300

    Aula Operadores

---
+++ Tipos-Variaveis/src/OperadoresUnarios.java
@@ -0,0 +1,30 @@
+public class OperadoresUnarios {
+    public static void main(String[] args) {
+        
+        int numero = 5;   // EX 02
+
+        // X repeticao
+        //numero ++;
+        //numero = numero +1;
+       System.out.println( ++ numero);
+       System.out.println(numero);
+
+        // imprimindo o numero negativo 
+         //System.out.println(- numero);    // EX 01
+         
+         // incremeto numero em mais 1 numero, imprime 6 numero ++;
+         //System.out.println(numero);
+
+         // incrementado numero em mais 1 numero, imprime 7
+         //System.out.println(numero ++); // ops algo de errado não está certo
+
+        // System.out.println(numero);// agora sim, numero virou 7
+
+          //ordem de precedencia conta aqui
+          // System.out.println(++ numero);
+
+           //boolean verdadeiro = true;
+
+        //System.out.println("Inverteu " + !verdadeiro); 
+    }
+}
